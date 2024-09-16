a5f88e9d8ebdce26b1f1e680e886d4b602a91f34
user name <eduardasm@dcc.ufrj.br>
2024-09-16 15:50:22 -0300

    add codigo preliminar usando cond, mutex e variavel impresso

--- lab05/soma-lock-sync.c
+++ lab05/soma-lock-sync.c
@@ -1,13 +1,17 @@
 /* Disciplina: Programacao Concorrente */
-/* Prof.: Silvana Rossetto */
-/* Codigo: Comunicação entre threads usando variável compartilhada e exclusao mutua com bloqueio */
+/* Codigo: O objetivo deste Laboratório é introduzir o mecanismo de sincronização por condição
+usando variaveis de condição da biblioteca Pthread. */
 
 #include <stdio.h>
 #include <stdlib.h> 
 #include <pthread.h>
 
-long int soma = 0; //variavel compartilhada entre as threads
-pthread_mutex_t mutex; //variavel de lock para exclusao mutua
+
+long int qt_mul = 0;
+int impresso = 0;
+long int soma = 0;
+pthread_mutex_t mutex; 
+pthread_cond_t cond;
 
 //funcao executada pelas threads
 void *ExecutaTarefa (void *arg) {
@@ -15,13 +19,21 @@ void *ExecutaTarefa (void *arg) {
   printf("Thread : %ld esta executando...\n", id);
 
   for (int i=0; i<100000; i++) {
-     //--entrada na SC
+
      pthread_mutex_lock(&mutex);
-     //--SC (seção critica)
-     soma++; //incrementa a variavel compartilhada 
-     //--saida da SC
+     soma++;  // incrementa o valor de soma
+     if(soma%10 == 0){ // ao encontrar um múltiplo de 10
+       impresso = 0; // marca que ainda não foi impresso
+       pthread_cond_signal(&cond);  // avisa p/  thread extra que ela pode imprimir
+       while(impresso == 0){ // aguarda até que seja impresso
+         pthread_cond_wait(&cond, &mutex);
+       }
+       impresso = 0;
+     }
      pthread_mutex_unlock(&mutex);
+
   }
+
   printf("Thread : %ld terminou!\n", id);
   pthread_exit(NULL);
 }
@@ -29,18 +41,26 @@ void *ExecutaTarefa (void *arg) {
 //funcao executada pela thread de log
 void *extra (void *args) {
   printf("Extra : esta executando...\n");
+  pthread_mutex_lock(&mutex);
   for (int i=0; i<10000; i++) {
-     if (!(soma%10)) //imprime se 'soma' for multiplo de 10
-        printf("soma = %ld \n", soma);
+    if (qt_mul < 20){
+      printf("soma = %ld \n", soma);
+      qt_mul++;
+      impresso = 1;
+      pthread_cond_signal(&cond);
+    } 
+    pthread_mutex_unlock(&mutex);   
+      
   }
+
   printf("Extra : terminou!\n");
   pthread_exit(NULL);
 }
 
 //fluxo principal
 int main(int argc, char *argv[]) {
-   pthread_t *tid; //identificadores das threads no sistema
-   int nthreads; //qtde de threads (passada linha de comando)
+   pthread_t *tid; 
+   int nthreads; 
 
    //--le e avalia os parametros de entrada
    if(argc<2) {
@@ -53,8 +73,9 @@ int main(int argc, char *argv[]) {
    tid = (pthread_t*) malloc(sizeof(pthread_t)*(nthreads+1));
    if(tid==NULL) {puts("ERRO--malloc"); return 2;}
 
-   //--inicilaiza o mutex (lock de exclusao mutua)
+   //--inicilaiza o mutex (lock de exclusao mutua) e a variável de condição
    pthread_mutex_init(&mutex, NULL);
+   pthread_cond_init(&cond, NULL);
 
    //--cria as threads
    for(long int t=0; t<nthreads; t++) {
@@ -63,7 +84,7 @@ int main(int argc, char *argv[]) {
      }
    }
 
-   //--cria thread de log
+   //--cria thread de print
    if (pthread_create(&tid[nthreads], NULL, extra, NULL)) {
       printf("--ERRO: pthread_create()\n"); exit(-1);
    }
@@ -75,8 +96,9 @@ int main(int argc, char *argv[]) {
      } 
    } 
 
-   //--finaliza o mutex
+   //--finaliza o mutex e a cond
    pthread_mutex_destroy(&mutex);
+   pthread_cond_destroy(&cond);
    
    printf("Valor de 'soma' = %ld\n", soma);
 
