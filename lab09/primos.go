bb9b3ad2f0b482458fac750bda6d9b2d6a8e0bbf
eduardamarks1 <eduardasm@dcc.ufrj.br>
2024-10-30 20:28:57 -0300

    add abordagem concorrente usando canais e comparacao

--- lab09/primos.go
+++ lab09/primos.go
@@ -4,10 +4,16 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"sync"
 	"time"
 )
 
 const N = 10 // quantidade de números na sequência a ser gerada
+const M = 3  // número de goroutines
+
+// variáveis para contar primos encontrados em cada abordagem
+var countPrimosSequencial int
+var countPrimosConcorrente int
 
 // função utilizada para verificar a primalidade de um número
 func ehPrimo(n int) int {
@@ -28,11 +34,25 @@ func ehPrimo(n int) int {
 	return 1
 }
 
-// checa quais números no array são primos de forma sequencial para fazer um sanity check
+// checa quais números no array são primos de forma sequencial
 func ehPrimoSequencial(array []int) {
 	for _, num := range array {
 		if ehPrimo(num) == 1 {
 			fmt.Printf("%d é primo\n", num)
+			countPrimosSequencial++
+		} else {
+			fmt.Printf("%d não é primo\n", num)
+		}
+	}
+}
+
+// função utilizada para verificar a primalidade dos números recebidos pelo canal (abordagem concorrente)
+func processarNumeros(numeros <-chan int, wg *sync.WaitGroup) {
+	defer wg.Done() // decrementa o WaitGroup quando a goroutine termina
+	for num := range numeros {
+		if ehPrimo(num) == 1 {
+			fmt.Printf("%d é primo\n", num)
+			countPrimosConcorrente++
 		} else {
 			fmt.Printf("%d não é primo\n", num)
 		}
@@ -42,17 +62,40 @@ func ehPrimoSequencial(array []int) {
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	min, max := 0, 100 // intervalo de valores que podem ser gerados
+	min, max := 0, 100          // intervalo de valores que podem ser gerados
+	numeros := make(chan int)   // canal para enviar os números
+	sequencia := make([]int, N) // array para armazenar os números gerados
 
-	array := make([]int, N) // cria o slice que vai guardar os números da sequência
+	var wg sync.WaitGroup
 
-	// gera os números da sequência e guarda no slice
-	for i := 0; i < N; i++ {
-		array[i] = rand.Intn(max-min) + min
+	// inicia M goroutines para processar os números
+	for i := 0; i < M; i++ {
+		wg.Add(1)
+		go processarNumeros(numeros, &wg)
 	}
 
-	fmt.Println("Sequência de números aleatórios:", array)
+	// gera os números da sequência e envia para o canal
+	for i := 0; i < N; i++ {
+		num := rand.Intn(max-min) + min
+		sequencia[i] = num // guarda os números gerados em um array
+		numeros <- num     // envia número para o canal
+	}
 
-	// chama a função que verifica quais números da sequência são primos
-	ehPrimoSequencial(array)
+	close(numeros) // fecha o canal após enviar todos os números
+
+	wg.Wait() // espera todas as goroutines terminarem
+
+	ehPrimoSequencial(sequencia)
+
+	fmt.Print(sequencia)
+
+	if countPrimosSequencial != countPrimosConcorrente {
+		println("As soluções sequencial e concorrente erraram a primalidade de pelo menos um número :(")
+	} else {
+		println("As soluções sequencial e concorrente encontraram a mesma quantidade de primos :)")
+	}
+
+	fmt.Printf("Quantidade de primos encontrados na abordagem sequencial: %d\n", countPrimosSequencial)
+	fmt.Printf("Quantidade de primos encontrados na abordagem concorrente: %d\n", countPrimosConcorrente)
+
 }
